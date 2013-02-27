796dc6580fbe161f5f4524017f386e02135dd6a5
nightmarelin <nightmarelin@gmail.com>
2013-02-27 22:11:12 +0800

    Initial Commit

---
+++ myoschina/MOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOAppDelegate.h
+//  myoschina
+//
+//  Created by user on 13-2-27.
+//  Copyright (c) 2013年 iso1030. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ myoschina/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myoschina
+//
+//  Created by user on 13-2-27.
+//  Copyright (c) 2013年 iso1030. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MOAppDelegate class]));
+    }
+}
