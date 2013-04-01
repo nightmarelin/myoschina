37bc013bfc07ec7fe40d1a44042174c3c8220084
nightmarelin <nightmarelin@gmail.com>
2013-04-01 22:49:42 +0800

    oh yeah

---
+++ myoschina/DataModel/MONews.h
@@ -0,0 +1,13 @@
+//
+//  MONews.h
+//  myoschina
+//
+//  Created by user on 13-3-31.
+//  Copyright (c) 2013年 iso1030. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MONews : NSObject
+
+@end
---
+++ myoschina/DataModel/MONews.m
@@ -0,0 +1,13 @@
+//
+//  MONews.m
+//  myoschina
+//
+//  Created by user on 13-3-31.
+//  Copyright (c) 2013年 iso1030. All rights reserved.
+//
+
+#import "MONews.h"
+
+@implementation MONews
+
+@end
